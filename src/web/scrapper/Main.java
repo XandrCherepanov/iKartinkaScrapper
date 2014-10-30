d0668a1c5a130f862e86354ff6d709e9d0c04622
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2014-10-30 21:01:07 +0500

    Exit from application

--- src/web/scrapper/Main.java
+++ src/web/scrapper/Main.java
@@ -11,12 +11,13 @@ import java.util.Scanner;
 public class Main {
 
     private static void showMainMenu(Map<Integer, Category> categories) {
-        System.out.println("Select option: ");
         System.out.println("1 - Refresh categories");
 
         for (Map.Entry<Integer, Category> entry : categories.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue().getName());
         }
+        System.out.println("q - Quit");
+        System.out.println("Select option [q]: ");
     }
 
     public static void main(String[] args) {
@@ -26,14 +27,18 @@ public class Main {
             Downloader downloader = new Downloader();
             Scanner scanner = new Scanner(System.in);
 
-            int input = 1;
+            String input = "q";
             do {
                 showMainMenu(downloader.getCategories());
-                input = scanner.nextInt();
-                if (input == 1) {
+                input = scanner.nextLine().trim();
+
+                if (input.equals("q") || input.equals("")) break;
+
+                int command = Integer.parseInt(input);
+                if (command == 1) {
                     downloader.refreshCategories();
                 }
-            } while (input == 1);
+            } while (!input.equals("q"));
 
         } catch (IOException|ClassNotFoundException|JauntException e) {
             System.err.println(e.getMessage());
